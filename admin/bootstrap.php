bd06d9db92446702acfd3063292ac13127b97c34
ChristopherZinger <christopher@van-ons.nl>
2021-06-03 22:56:29 +0200

    register title-tag and menus

--- admin/bootstrap.php
+++ admin/bootstrap.php
@@ -5,6 +5,25 @@ function rc_register_styles ()
 {
   $version = wp_get_theme()->get( 'Version' );
   wp_enqueue_style( 'rc-styles', get_template_directory_uri() . '/style.css', array(), $version, 'all' );
-};
-
+}
 add_action( 'wp_enqueue_scripts', 'rc_register_styles');
+
+
+// theme support: automatic title
+function rc_theme_support () 
+{
+  add_theme_support( 'title-tag' );
+}
+add_action( 'after_setup_theme', 'rc_theme_support' );
+
+// menus
+function rc_menus ()
+{
+  $locations = array(
+    'primary' => 'Primary Menu',
+    'footer' => 'Footer Meny'
+  );
+
+  register_nav_menus($locations);
+}
+add_action( 'init', 'rc_menus' );
