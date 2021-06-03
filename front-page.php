817da089564418f1953ec59b7bfe5ba497b071fd
ChristopherZinger <christopher@van-ons.nl>
2021-06-03 22:55:43 +0200

    display posts on front-page

--- front-page.php
+++ front-page.php
@@ -2,6 +2,19 @@
 
   <main>
     <h3>Main</h3> 
+    <?php
+    if ( have_posts() )
+    {
+      while ( have_posts() ) 
+      {
+        the_post();
+        the_content();
+      } 
+    } else {
+      echo '<p>There are no posts here.</p>';
+    }
+
+    ?>
   </main>
 
 <?php get_footer(); ?>
