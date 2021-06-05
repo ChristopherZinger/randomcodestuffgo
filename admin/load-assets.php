55e352b06b29b0f99bf497430d9cf38052b15ac6
ChristopherZinger <christopher@van-ons.nl>
2021-06-05 15:56:26 +0200

    add google fonts

--- admin/load-assets.php
+++ admin/load-assets.php
@@ -1,5 +1,13 @@
 <?php
 
+
+function wpb_add_google_fonts() {
+   wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css2?family=Inter&display=swap', false );
+}
+   
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
+
+
 function rc_load_styles () {
   $version = wp_get_theme()->get( 'Version' );
   wp_enqueue_style(
