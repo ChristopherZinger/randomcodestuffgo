821d0012a94e93835e3f5703f39dba181612ec16
ChristopherZinger <christopher@van-ons.nl>
2021-06-05 12:27:34 +0200

    bundle styles with gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import yargs from 'yargs';
+import gulpif from 'gulp-if';
+import cleanCSS from 'gulp-clean-css'
+
+const PRODUCTION = yargs.argv.prod;
+
+gulp.task('default', mainTask);
+
+
+export const styles = () => {
+  return gulp.src('src/scss/bundle.scss' )
+    .pipe( sass().on('error', sass.logError ))
+    .pipe( gulpif(
+      PRODUCTION, cleanCSS({ compatibility: 'i8' } )
+    ))
+    .pipe( gulp.dest('dist/assets/css' ))
+}
+
+function mainTask (done) {
+  console.log("Gulp: Main Task - running all tasks.")
+  done();
+}
