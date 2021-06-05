43cf125f473cacc28c0ac71197d0d0ead12fc3dd
ChristopherZinger <christopher@van-ons.nl>
2021-06-05 12:55:40 +0200

    add sourcemaps for gulp styles bundling

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,8 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import yargs from 'yargs';
 import gulpif from 'gulp-if';
-import cleanCSS from 'gulp-clean-css'
+import cleanCSS from 'gulp-clean-css';
+import sourcemaps from 'gulp-sourcemaps';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -10,9 +11,15 @@ gulp.task('default', mainTask);
 
 export const styles = () => {
   return gulp.src('src/scss/styles.scss' )
+    .pipe( gulpif(
+      !PRODUCTION, sourcemaps.init()
+    ))
     .pipe( sass().on('error', sass.logError ))
     .pipe( gulpif(
       PRODUCTION, cleanCSS({ compatibility: 'i8' } )
+    ))
+    .pipe( gulpif(
+      !PRODUCTION, sourcemaps.write()
     ))
     .pipe( gulp.dest('dist/assets/css' ))
 }
