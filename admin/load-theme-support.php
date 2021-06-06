24469b619789c163455b73905b5b2bc6ce0b8086
ChristopherZinger <christopher@van-ons.nl>
2021-06-06 12:54:12 +0200

    add theme support post-thumbnails - page; align-wide, align-full

--- admin/load-theme-support.php
+++ admin/load-theme-support.php
@@ -7,6 +7,13 @@ function rc_theme_support ()
 
   // removes margin-top 32px !important from html element.
   add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
+
+  // thumbnails for pages
+  add_theme_support( 'post-thumbnails', 'page' );
+
+  // alignwide and align full
+  add_theme_support( 'align-wide' );
+  add_theme_support( 'align-full' );
 }
 
 add_action( 'after_setup_theme', 'rc_theme_support' );
