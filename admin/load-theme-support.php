387050094abd38453e357bb5c2c240493c08b483
ChristopherZinger <christopher@van-ons.nl>
2021-06-06 10:35:09 +0200

    remove html admin margin

--- admin/load-theme-support.php
+++ admin/load-theme-support.php
@@ -1,9 +1,12 @@
 <?php
 
-// theme support: automatic title
 function rc_theme_support () 
 {
+  // theme support: automatic title
   add_theme_support( 'title-tag' );
+
+  // removes margin-top 32px !important from html element.
+  add_theme_support( 'admin-bar', array( 'callback' => '__return_false' ) );
 }
 
 add_action( 'after_setup_theme', 'rc_theme_support' );
