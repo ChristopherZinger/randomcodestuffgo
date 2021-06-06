854ecee65ad01fde9dd875e8fd549c36785cc392
ChristopherZinger <christopher@van-ons.nl>
2021-06-06 12:54:51 +0200

    contact page WIP; basic styles and structure

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -2,8 +2,26 @@
 /*
 Template Name: Contact
 */
+
+  $args = array( 'numberposts' => -1, 'category_name' => 'contact' );
+  $posts = get_posts( $args );  
 ?>
 
-<?php get_template_part('template-parts/menus/primary-menu'); ?>
+<?php get_header(); ?>
 
-<h1>Contact</h1>
+<main class="page-contact">
+  <section>
+    <?php the_content(); ?>
+
+    <?php
+      foreach($posts as $contactPost) {
+        echo get_the_title($contactPost);
+        echo get_the_content( null, false, $contactPost);
+      }
+    ?>
+  </section>
+
+  <aside class="page-contact__aside-image">
+    <img src="<?php echo get_the_post_thumbnail_url( $post, 'large' ); ?>" alt="image" />
+  </aside>
+</main>
