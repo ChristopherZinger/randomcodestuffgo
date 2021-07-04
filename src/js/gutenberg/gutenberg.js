2df2b0d50f0daf82d70ca86ad7dd48e3ee49c634
ChristopherZinger <christopher@van-ons.nl>
2021-07-04 12:28:54 +0200

    gutenberg.js

--- src/js/gutenberg/gutenberg.js
+++ src/js/gutenberg/gutenberg.js
@@ -1,1 +1,26 @@
-console.log('Gutenberg scripts')
+import initBlockStyles from './config/blockStyles'
+import listThreeSeries from './blocks/list-three-series'
+import { colors } from './settings'
+
+const { __ } = wp.i18n
+const { setCategories, getCategories } = wp.blocks
+
+// unregisterBlockStyle doesn't work inside wp.domReady
+// https://github.com/WordPress/gutenberg/issues/25330
+
+window.onload = () => {
+    initBlockStyles()
+}
+
+// Add Van Ons block category with custom icon
+setCategories([
+    {
+        slug: 'rc',
+        title: __('Random Code', 'rc'),
+        icon: 'buddicons-replies',
+    },
+    ...getCategories().filter(({ slug }) => slug !== 'rc'),
+])
+
+// Register blocks
+listThreeSeries()
