b62ded06e61594dbfbdab3db40b5e5d903ccb6c3
Christopher <christopher@van-ons.nl>
2021-08-08 22:12:51 +0200

    social media icons

---
+++ template-parts/part-social-media.php
@@ -0,0 +1,25 @@
+<div class="social-media">
+	<div class="social-media__icon social-media__share">
+		<a href=""></a>
+	</div>
+
+	<div class="social-media__icon social-media__bookmark">
+		<a href=""></a>
+	</div>
+
+	<div class="social-media__icon social-media__phone">
+		<a href=""></a>
+	</div>
+
+	<div class="social-media__icon social-media__email">
+		<a href=""></a>
+	</div>
+
+	<div class="social-media__icon social-media__github">
+		<a href=""></a>
+	</div>
+
+	<div class="social-media__icon social-media__linkedin">
+		<a href=""></a>
+	</div>
+</div>
