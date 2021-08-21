b1d8495abc477bf321fb5d0911c6703301e39694
Christopher <christopher@van-ons.nl>
2021-08-21 09:02:19 +0200

    use homepage template

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -5,11 +5,20 @@ Template Name: Home
 ?>
 
 <?php get_header(); ?>
-
   <main class="content">
-    <h3>Main</h3> 
+    <h1 class="home__header h1">Main</h1> 
 
-
+    <?php get_template_part('template-parts/part-tags'); ?>
+    <?php get_template_part('template-parts/part', 'series') ?>
+    
+    <div class="grid-x grid-margin-x latest-posts section">
+      <?php get_template_part('template-parts/part', 'latest-posts'); ?>
+      <div class="home__social-media cell large-4 show-for-large">
+          <div>
+            <h2 class="h6">Social Media:</h2>
+          </div>
+          <?= get_template_part('template-parts/part', 'social-media'); ?>
+      </div>
+    </div>
   </main>
-
 <?php get_footer(); ?>
