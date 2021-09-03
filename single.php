b28afa6626daa0b2ccbf76170a46ad4e692307af
Christopher <christopher@van-ons.nl>
2021-09-03 08:45:27 +0200

    add sidebar to single.php

--- single.php
+++ single.php
@@ -5,7 +5,20 @@
         'title' => get_the_title(),
         'excerpt' => get_the_excerpt()
     ] ) ?>
-    <?php the_content() ?>
+
+    <div class="grid-x grid-margin-x section">
+        <div class="cell medium-1 large-2 show-for-medium"></div>
+        <div class="rc-post-content cell small-12 medium-7 large-6">
+            <?= the_content() ?>
+        </div>
+        <div class="cell large-1 show-for-large"></div>
+        <div class="cell medium-4 large-3 show-for-medium">
+            <div class="side-bar side-bar--sticky">
+                <?= get_template_part('template-parts/part', 'social-media'); ?>
+            <div class="sidebar-location-navigation"></div>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php
