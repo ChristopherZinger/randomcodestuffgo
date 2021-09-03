96454ab07786e81d868f7215ee01165a22460da2
Christopher <christopher@van-ons.nl>
2021-09-03 08:10:54 +0200

    selectable categories

--- template-parts/blocks/selectable-categories.php
+++ template-parts/blocks/selectable-categories.php
@@ -6,13 +6,17 @@
 ?>
 
 <div class="selectable-categories">
-	<div>
-		<input class="selectable-categories" type="text">
+	<div class="selectable-categories__form-wrapper">
+		<form id="searchform">
+			<input id="s" class="selectable-categories__input" type="text" placeholder="<?= __('Search for series.', 'rc') ?>">
+			<div id="searchsubmit" class="selectable-categories__clear-search-btn">clear
+			</div>
+		</form>
 	</div>
 
 	<div>
 		<?php foreach ($categories as $cat) { ?>
-			<div class="selectable-categories__section card" data-slug="<?= esc_attr(strtolower($cat->name)) ?>">
+			<div class="selectable-categories__item card" data-title="<?= esc_attr(strtolower($cat->name)) ?>">
 				<a class="" href="<?= get_category_link( $cat ) ?>">
 					<h6 class="selectable-categories__title"><?= esc_html($cat->name) ?></h6>
 					<p class="selectable-categories__excerpt"><?= esc_html($cat->description) ?></p>
