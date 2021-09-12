8bd3f43cd87d7c20d4031bcd818c45a56df29412
Christopher <christopher@van-ons.nl>
2021-09-12 10:59:17 +0200

    gist

--- src/js/theme/app.js
+++ src/js/theme/app.js
@@ -3,10 +3,14 @@ import HeaderSubmenu from './components/headerSubmenu'
 import HeaderDarkMode from './components/headerDarkMode'
 import Accordion from './components/accordion'
 import LocationNavigation from './components/locationNavigation'
+import Gist from './components/gist'
 import SelectableCategories from './components/selectableCategories'
 import PageSeries from './components/pageSeries'
+import { seriesSlider } from './components/seriesSlider'
 
 // todo : document on load
+
+seriesSlider()
 
 new Accordion()
 new Header()
@@ -15,4 +19,5 @@ new HeaderDarkMode()
 new LocationNavigation()
 new PageSeries()
 new SelectableCategories()
+new Gist()
 
