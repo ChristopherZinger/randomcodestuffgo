35b802d0c07ca475b31fb98c987b8131ec3b6d50
ChristopherZinger <christopher@van-ons.nl>
2021-10-02 10:46:36 +0200

    remove axios and use fetch

--- src/js/theme/API/getPostsByCategoryId.js
+++ src/js/theme/API/getPostsByCategoryId.js
@@ -1,20 +1,20 @@
+export const getPostsByCategoryId = async (keyword, categories, per_page = 10, page = 1) => {
+    const params = {
+        search: keyword,
+        _fields: 'id,title,date,excerpt,link,tags',
+        categories,
+        per_page,
+        page,
+    }
+    const decodedParams = '?' + new URLSearchParams(params).toString()
+    const url = '/wp-json/wp/v2/posts' + decodedParams
 
-import axios from 'axios'
-
-export const getPostsByCategoryId = async (keyword, categories, per_page=10, page=1) => {
-	try{
-		const { data } = await axios.get('/wp-json/wp/v2/posts', {
-			params: {
-				search: keyword,
-				'_fields' : 'id,title,date,excerpt,link,tags',
-				categories,
-				per_page,
-				page
-			}
-		})
-		return data 
-	} catch (err) {
-		console.warn(err)
-		return []
-	}
+    try {
+        const result = await fetch(url, { method: 'GET' })
+        console.log('fetch: ', result)
+        return result
+    } catch (err) {
+        console.warn(err)
+        return []
+    }
 }
