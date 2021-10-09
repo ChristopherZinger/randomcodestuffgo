443f53d93eb1ace4ef8f2fe939240a1519d8426a
ChristopherZinger <christopher@van-ons.nl>
2021-10-09 13:29:12 +0200

    remove social media from home

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -20,12 +20,6 @@ Template Name: Home
     
     <div class="grid-x grid-margin-x latest-posts section">
       <?php get_template_part('template-parts/part', 'latest-posts'); ?>
-      <div class="home__social-media cell large-4 show-for-large">
-          <div>
-            <h2 class="h6">Social Media:</h2>
-          </div>
-          <?= get_template_part('template-parts/part', 'social-media'); ?>
-      </div>
     </div>
   </main>
 <?php get_footer(); ?>
